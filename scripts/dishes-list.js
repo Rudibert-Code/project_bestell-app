1f043dd83f0b0bf80f9619db5769a40a558af76c
Bjoern <onoofelogmail.com>
2026-03-17 16:22:19 +0100

    dishes-list : - created food list JSON

---
+++ scripts/dishes-list.js
@@ -0,0 +1,77 @@
+let myDishes = [
+    {
+        "Category":"Burger",
+        "Dish": [
+            {
+                "Name":"Veggi mushroom black burger",
+                "Price": 16.90,
+                "Details": "Mixed green salad, tomatoes, edamame, mushrooms"
+            },  
+            {
+                "Name":"All meat burger",
+                "Price": 15.90,
+                "Details": "Beef, bacon, dill pickles, smoked cheese, ketchup, BBQ sauce"
+            },
+            {
+                "Name":"Beef red burger",
+                "Price": 14.90,
+                "Details": "Beef, cheese tomatoes, onions"
+            },
+            {
+                "Name":"Blg chicken burger",
+                "Price": 15.99,
+                "Details": "Chicken, cheese, tomatoes, lettuce, onions, bell peppers"
+            },
+        ]
+    },
+    {
+        "Category":"Pizza",
+        "Dish": [
+            {
+                "Name":"Margherita",
+                "Price": 11.90,
+                "Details": "Tomato sauce, mozarella"
+            },  
+            {
+                "Name":"Chorio",
+                "Price": 13.50,
+                "Details": "Tomato slices, mozarella, chorizo"
+            },
+            {
+                "Name":"Fungi",
+                "Price": 12.99,
+                "Details": "Red onions, olives, button mushrooms, mozarella"
+            },
+            {
+                "Name":"Quatro fromaggi with chicken",
+                "Price": 15.50,
+                "Details": "Chicken, mozarella, gorgonzola, fontina, parmigiano reggiano"
+            },
+        ]
+    },
+    {
+        "Category":"Salad",
+        "Dish": [
+            {
+                "Name":"Warm beef arugula salad",
+                "Price": 16.90,
+                "Details": "Beef, arugula, field salad, greek feta, cherry tomatos, sun-dired tomatoes, balsamic-vinegar dressing"
+            },  
+            {
+                "Name":"Mini green salad",
+                "Price": 7.90,
+                "Details": "Green salad, cucumber, carrots, parsley, radish"
+            },
+            {
+                "Name":"Sea food salad",
+                "Price": 16.00,
+                "Details": "Mixed greens, cherry tomatoes, red onions, mussels, squid rings, shrimps, dijon mustardlemon dressing with dill"
+            },
+            {
+                "Name":"Vegan green salad with tofu",
+                "Price": 14.90,
+                "Details": "Green salad, cherry tomatoes, cucumber, baby spinach, edamame, radishes, bittercress, tofu, peanuts"
+            },
+        ]
+    },
+]
